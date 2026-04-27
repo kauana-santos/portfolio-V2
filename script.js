1f02ca4cf194054b7428d3d6d32767137c3bfed2
kauana-santos <160909870+kauana-santos@users.noreply.github.com>
2026-04-27 14:20:07 -0300

    Alteração js
    Iniciando listagem de projetos para o portfólio

--- script.js
+++ script.js
@@ -10,3 +10,28 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+
+const projetos = [
+    {
+        projectName: "notelab",
+        projectImg: " ",
+        projectDesc:"Projeto relizado com java Spring web"
+    },
+    {
+        projectName: "Organo",
+        projectImg: " ",
+        projectDesc:"Projeto relizado com java Spring web"
+    },
+    {
+        projectName: "atividade Java",
+        projectImg: " ",
+        projectDesc:"Projeto relizado com java Spring web"
+    },
+    {
+        projectName: "notelab",
+        projectImg: " ",
+        projectDesc:"Projeto relizado com java Spring web"
+    },
+    // criar função para incluir os projetos no css
+
+]

41075980a6039cc95045d4150afaa3908a229660
kauana-sants <124754922+kauana-sants@users.noreply.github.com>
2026-05-22 13:38:05 -0300

    alteração projetos
    adicionando projetos via js.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const hamburger = document.querySelector(".hamburguer");
 const navMenu = document.querySelector(".nav-menu");
-const cardsMenu = document.querySelector(".cards-container");
+const cardsContainer = document.querySelector(".cards-container");
 
 hamburger.addEventListener("click", ()=>{
     hamburger.classList.toggle("active");
@@ -12,27 +12,53 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     navMenu.classList.remove("active");
 }))
 
-const project = [
+const projects = [
     {
         name: "notelab",
         img: " ",
+        link:"",
         desc:"Projeto relizado com java Spring web"
     },
     {
         name: "Organo",
         img: " ",
+        link:"",
         desc:"Projeto relizado com java Spring web"
     },
     {
         name: "atividade Java",
         img: " ",
+        link:"",
         desc:"Projeto relizado com java Spring web"
     },
     {
-        name: "notelab",
+        
+        name: "galeria",
         img: " ",
+        link:"",
         desc:"Projeto relizado com java Spring web"
     },
-    // criar função para incluir os projetos no css
 ]
 
+cardsContainer.innerHTML = "";
+
+const cardsHTML = projects.map(project => {
+    return `
+        <div class="cards">
+            <a href="${project.link}" target="_blank">
+                <img src="${project.img}" 
+                     alt="${project.name}" 
+                     class="projeto-foto">
+            </a>
+
+            <div class="projeto-descricao">
+                <h3>${project.name}</h3>
+                <p class="p-projetos">
+                    ${project.desc}
+                </p>
+            </div>
+        </div>
+    `;
+}).join("");
+
+cardsContainer.innerHTML = cardsHTML;
